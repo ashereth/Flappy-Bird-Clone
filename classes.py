90d09fd4f29511617d1ffabf155bb1f2d597e1e1
Asher Etherington <asherce21@gmail.com>
2023-12-14 21:16:05 -0800

    added a lot of comments

--- classes.py
+++ classes.py
@@ -2,33 +2,37 @@ import pygame
 from variables import screen_width, screen, screen_height, sky_surface, ground_floor, ground_rect, ground_surface, clock, speed
 ### classes used throughout program
 
+# main player bird class
 class Bird(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
+        # get the image for the bird and resize it
         original_bird_image = pygame.image.load('graphics/bird/flappy.png').convert_alpha()
         original_bird_image = pygame.transform.rotozoom(original_bird_image, 0, .3)
-        self.index = 0
         self.image = original_bird_image
 
         # Set the rect based on the image size
         self.rect = self.image.get_rect(midbottom=(screen_width // 2, 300))
 
-        # Adjust the rect to match the size of the scaled image
-
+        #set gravity to be 0
         self.gravity = 0
 
     def player_input(self):
+        #if space is pressed change gravity to -12 which with make bird 'jump'
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE]:
             self.gravity = -12
 
     def apply_gravity(self):
+        # gravity should be constantly increasing to make it fall faster
         self.gravity += 1
         self.rect.y += self.gravity
+        # make sure bird doesnt fall through ground
         if self.rect.y >= 560:
             self.rect.y = 560
 
     def update(self):
+        # call all needed function that change state of bird in update
         self.player_input()
         self.apply_gravity()
 
@@ -37,6 +41,7 @@ class Bird(pygame.sprite.Sprite):
 class Pipe_bottom(pygame.sprite.Sprite):
     def __init__(self, height) -> None:
         super().__init__()
+        #get image and scale it
         self.bottom = pygame.image.load('graphics/pipe/pipe.png').convert_alpha()
         self.bottom = pygame.transform.rotozoom(self.bottom, 0, .4)
 
@@ -45,7 +50,7 @@ class Pipe_bottom(pygame.sprite.Sprite):
     
     def destroy(self):
         if self.rect.x <= -100: 
-            self.kill()#destroys the Obstacle sprite
+            self.kill()#destroys the Obstacle sprite if too far left
 
     def update(self):
         #move the pipe to the left
--- main.py
+++ main.py
@@ -34,11 +34,12 @@ pipe_group.add(pipe_bottom )
 all_sprites = pygame.sprite.Group()
 all_sprites.add(pipe_bottom, pipe_top, bird)
 
-# Timer for pipes
+# Timer for spawning pipes
 pipe_timer = pygame.USEREVENT + 1
 pygame.time.set_timer(pipe_timer,3000)
 
 
+#function for determining if the bird is colliding with any pipes
 def collision_sprite():
 	if pygame.sprite.spritecollide(bird.sprite, pipe_group,False):#checks if player sprite is colliding with anything in the obstacle group
 		pipe_group.empty()#empty obstace group so that next game starts fresh
@@ -53,25 +54,29 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-
+        # for spawning new pipes 
         if game_active:
             if event.type == pipe_timer:
                 height = randint(240, 550)
                 pipe_group.add(Pipe_bottom(height), Pipe_top(height))
-        else:
+        else: # start the game if spacebar is pressed and game is not active
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
                 score = 0
+
     if game_active:
+        # draw the ground, sky, and score making sure to render a new score every frame to keep it updated
         screen.blit(sky_surface,(0,0))
         screen.blit(ground_surface, ground_rect)
         score_surf = test_font.render(f'{score}',False,"black")
         score_rect = score_surf.get_rect(center = (screen_width//2+10,50))
         
+        #move ground to the left and if it goes too far reset it so that it can loop forever
         ground_rect.x-=speed
         if ground_rect.x <= -200:
             ground_rect.x+=200
 
+        #draw the bird and pipes
         bird.draw(screen)
         bird.update()
         pipe_group.draw(screen)
@@ -83,16 +88,16 @@ while True:
              if pipe.rect.x == 270:
                   score+= 1
 
-
+        # check for collisions and update game_active
         game_active = collision_sprite()
 
-    else:
+    else:# if game is not active show a basic menu screen
         screen.fill((94,129,162))
         score_message = test_font.render(f'Your score: {score}',False,(111,196,169))
         screen.blit(menu_bird_surf, menu_bird_rect)
         score_message_rect = score_message.get_rect(center = (screen_width//2,330))
         screen.blit(game_name,game_name_rect)
-
+        # if the last score was 0 print a message otherwise print the last score
         if score == 0: screen.blit(game_message,game_message_rect)
         else: screen.blit(score_message,score_message_rect)
     
